f37afc5933c9bff613c8ad68339e39ea080e13ba
arsalanses <dr.sefidgar@gmail.com>
2018-09-11 22:11:25 +0430

    delete errors

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from time import sleep
 import datetime
 import csv
 
-date = datetime.datetime.today().strftime('%Y-%m-%d')
+DATE = datetime.datetime.today().strftime('%Y-%m-%d')
 MAX_USAGE = 1024
 
 def clear(): 
@@ -17,27 +17,25 @@ def clear():
 
 old_traffic = 0
 fieldnames = ['date', 'traffic']
-dict_list = []
+db = []
+
 while True:
     traffic = psutil.net_io_counters().bytes_sent / 1000000 + psutil.net_io_counters().bytes_recv / 1000000
     if old_traffic != traffic:
         old_traffic = traffic
-        clear()
+    clear()
     print("Today usage: {0:.2f}".format(old_traffic))
         
-        # with open('DB.csv', 'r') as outfile:
-        #     reader = csv.DictReader(outfile)
-    reader = csv.DictReader(open('DB.csv', 'r'))
-    for line in reader:
-        dict_list.append(line)
-            # with open('DB.csv', 'w') as infile:
-            #     writer = csv.DictWriter(infile, fieldnames = fieldnames)
-            #     data["date"] = date
-            #     data["traffic"] = old_traffic
-            #     # writer.writeheader()
-            #     writer.writerow(data)
+    with open('DB.csv', mode = 'r') as outfile:
+        reader = csv.DictReader(outfile)
+        db = list(reader)
+        outfile.close()
 
-    print(dict_list)
+    with open('DB.csv', 'w') as infile:
+        writer = csv.DictWriter(infile, fieldnames = fieldnames)
+        writer.writeheader()
+        writer.writerow({"date": DATE, "traffic": old_traffic})
+        infile.close()
 
     if old_traffic > MAX_USAGE:
         print('Today you spend more than {0:.2f} MegaBytes!'.format(old_traffic))
