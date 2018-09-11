6163e972f1d2cf12c56eef4213deb9ae44cd705f
arsalanses <dr.sefidgar@gmail.com>
2018-09-11 19:00:23 +0430

    make csv file

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import psutil
 from os import system, name
 from time import sleep
 import datetime
+import csv
 
 date = datetime.datetime.today().strftime('%Y-%m-%d')
 MAX_USAGE = 1024
@@ -15,7 +16,6 @@ def clear():
         _ = system('clear') 
 
 old_traffic = 0
-
 while True:
     traffic = psutil.net_io_counters().bytes_sent / 1000000 + psutil.net_io_counters().bytes_recv / 1000000
     if old_traffic != traffic:
@@ -23,11 +23,15 @@ while True:
         clear()
         print("Today usage: {0:.2f}".format(old_traffic))
         
-        f = open("DB.txt", "w")
-        tmp = f.read()
-        tmp[date] = old_traffic
-        f.write(tmp)
-        f.close()
+        with open('DB.csv', 'w') as infile:
+            writer = csv.writer(infile)
+            with open('DB.csv', 'r') as outfile:
+                reader = csv.reader(outfile)
+                data = dict(reader)
+                data[date] = old_traffic
+                for key, value in data.items():
+                    writer.writerow([key, value])
+                    print(data)
 
         if traffic > MAX_USAGE:
             print('Today you spend more than {0:.2f} MegaBytes!'.format(old_traffic))
