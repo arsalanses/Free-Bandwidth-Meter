e1f5322bcf3dcfbd992ae3584ca6e31bfbcab2c7
arsalanses <dr.sefidgar@gmail.com>
2018-09-11 17:25:34 +0430

    add usage alert

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import psutil
 from os import system, name
 from time import sleep
 
+MAX_USAGE = 1024
+
 def clear(): 
     # for windows 
     if name == 'nt': 
@@ -10,11 +12,13 @@ def clear():
     else: 
         _ = system('clear') 
 
-net = psutil.net_io_counters(pernic=True)['Wi-Fi'][1] / 1000000
+traffic = psutil.net_io_counters(pernic=True)['Wi-Fi'][1] / 1000000
 
 while 1:
-    if net != psutil.net_io_counters(pernic=True)['Wi-Fi'][1] / 1000000:
-        net = psutil.net_io_counters(pernic=True)['Wi-Fi'][1] / 1000000
+    if traffic != psutil.net_io_counters(pernic=True)['Wi-Fi'][1] / 1000000:
+        traffic = psutil.net_io_counters(pernic=True)['Wi-Fi'][1] / 1000000
         clear()
-        print("Megabytes_recv: {}".format(net))
+        print("Megabytes_recv: {}".format(traffic))
+        if traffic > MAX_USAGE:
+            print('Today you spend more than {} MegaBytes!'.format(traffic))
         sleep(5)
