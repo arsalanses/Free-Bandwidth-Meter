e0297b29689d7d1f213114da255993a2f8e789bb
arsalanses <dr.sefidgar@gmail.com>
2018-09-11 21:00:09 +0430

    add DictReader

--- app.py
+++ app.py
@@ -16,23 +16,29 @@ def clear():
         _ = system('clear') 
 
 old_traffic = 0
+fieldnames = ['date', 'traffic']
+dict_list = []
 while True:
     traffic = psutil.net_io_counters().bytes_sent / 1000000 + psutil.net_io_counters().bytes_recv / 1000000
     if old_traffic != traffic:
         old_traffic = traffic
         clear()
-        print("Today usage: {0:.2f}".format(old_traffic))
+    print("Today usage: {0:.2f}".format(old_traffic))
         
-        with open('DB.csv', 'w') as infile:
-            writer = csv.writer(infile)
-            with open('DB.csv', 'r') as outfile:
-                reader = csv.reader(outfile)
-                data = dict(reader)
-                data[date] = old_traffic
-                for key, value in data.items():
-                    writer.writerow([key, value])
-                    print(data)
+        # with open('DB.csv', 'r') as outfile:
+        #     reader = csv.DictReader(outfile)
+    reader = csv.DictReader(open('DB.csv', 'r'))
+    for line in reader:
+        dict_list.append(line)
+            # with open('DB.csv', 'w') as infile:
+            #     writer = csv.DictWriter(infile, fieldnames = fieldnames)
+            #     data["date"] = date
+            #     data["traffic"] = old_traffic
+            #     # writer.writeheader()
+            #     writer.writerow(data)
 
-        if traffic > MAX_USAGE:
-            print('Today you spend more than {0:.2f} MegaBytes!'.format(old_traffic))
-        sleep(5)
+    print(dict_list)
+
+    if old_traffic > MAX_USAGE:
+        print('Today you spend more than {0:.2f} MegaBytes!'.format(old_traffic))
+    sleep(5)
