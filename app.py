b19115674c44a9116c30d565e1ab8df8975eb007
arsalanses <dr.sefidgar@gmail.com>
2018-09-11 17:42:19 +0430

    make code readable :D

--- app.py
+++ app.py
@@ -12,13 +12,14 @@ def clear():
     else: 
         _ = system('clear') 
 
-traffic = psutil.net_io_counters().bytes_sent / 1000000 + psutil.net_io_counters().bytes_recv / 1000000
+old_traffic = 0
 
 while 1:
-    if traffic != psutil.net_io_counters().bytes_sent / 1000000 + psutil.net_io_counters().bytes_recv / 1000000:
-        traffic = psutil.net_io_counters().bytes_sent / 1000000 + psutil.net_io_counters().bytes_recv / 1000000
+    traffic = psutil.net_io_counters().bytes_sent / 1000000 + psutil.net_io_counters().bytes_recv / 1000000
+    if old_traffic != traffic:
+        old_traffic = traffic
         clear()
-        print("Today usage: {0:.2f}".format(traffic))
+        print("Today usage: {0:.2f}".format(old_traffic))
         if traffic > MAX_USAGE:
-            print('Today you spend more than {0:.2f} MegaBytes!'.format(traffic))
+            print('Today you spend more than {0:.2f} MegaBytes!'.format(old_traffic))
         sleep(5)
