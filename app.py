43a90934d4ab80c38d45e676b377dd800e0791ed
arsalanses <dr.sefidgar@gmail.com>
2018-09-11 17:14:55 +0430

    clear screen

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
 import psutil
+from os import system, name
 
-print("Megabytes_recv: {}".format(psutil.net_io_counters(pernic=True)['Wi-Fi'][1] / 1000000))
+def clear(): 
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+
+net = psutil.net_io_counters(pernic=True)['Wi-Fi'][1] / 1000000
+
+while 1:
+    if net != psutil.net_io_counters(pernic=True)['Wi-Fi'][1] / 1000000:
+        net = psutil.net_io_counters(pernic=True)['Wi-Fi'][1] / 1000000
+        clear()
+        print("Megabytes_recv: {}".format(net))
